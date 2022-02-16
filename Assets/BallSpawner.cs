54ef5b8bfb9af1a4b0d95c8d4d8b94edc11444b0
Kamiel <kamieldevisser@gmail.com>
2022-02-16 06:37:07 -0800

    add ball spawner

--- Assets/BallSpawner.cs
+++ Assets/BallSpawner.cs
@@ -7,6 +7,11 @@ public class BallSpawner : MonoBehaviour
     public Transform spawnPos;
     public GameObject ballPrefab;
 
+    private void Start()
+    {
+        SpawnNewBall();
+    }
+
     internal void SpawnNewBall()
     {
         Instantiate(ballPrefab, spawnPos.position, Quaternion.identity);
